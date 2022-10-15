1fd9b7d5608fda0d3ad13a48d9573972bd5f6a7e
Kim Yuen <kim.yuen@akqa.com>
2022-10-15 12:09:38 +1100

    wip

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,10 +2,10 @@ import { PageInfo } from "../typings";
 
 export const fetchPageInfo = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getPageInfo`);
-     // check res for error
-    // if (!res.ok) {
-    //     console.log(res.statusText);
-    // }
+    //  check res for error
+    if (!res.ok) {
+        console.log(res.statusText);
+    }
     const data = await res.json();
     const pageInfo: PageInfo = data.pageInfo;
     return pageInfo;
