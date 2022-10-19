3f741c772d53799945b4b3aebe3d542d843db29f
kim <kim_man@live.com>
2022-10-19 18:12:05 +1100

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import type { GetStaticProps } from 'next'
-import Head from 'next/head'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import About from '../components/About'
-import WorkExperience from '../components/WorkExperience'
-import Skills from '../components/Skills'
-import Projects from '../components/Projects'
-import Contact from '../components/Contact'
+import type { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import WorkExperience from '../components/WorkExperience';
+import Skills from '../components/Skills';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
 import Link from 'next/link';
-import { PageInfo, Experience, Skill, Project, Social } from '../typings'
-import { fetchPageInfo } from '../utils/fetchPageInfo'
-import { fetchExperience } from '../utils/fetchExperience'
-import { fetchSkills } from '../utils/fetchSkills'
-import { fetchProjects } from '../utils/fetchProjects'
-import { fetchSocials } from '../utils/fetchSocials'
+import { PageInfo, Experience, Skill, Project, Social } from '../typings';
+import { fetchPageInfo } from '../utils/fetchPageInfo';
+import { fetchExperience } from '../utils/fetchExperience';
+import { fetchSkills } from '../utils/fetchSkills';
+import { fetchProjects } from '../utils/fetchProjects';
+import { fetchSocials } from '../utils/fetchSocials';
 
 type Props = {
   pageInfo: PageInfo;
@@ -21,61 +21,66 @@ type Props = {
   skills: Skill[];
   projects: Project[];
   socials: Social[];
-}
+};
 
-const Home = ({pageInfo, experience, skills, projects, socials}: Props) => {
+const Home = ({ pageInfo, experience, skills, projects, socials }: Props) => {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
       <Head>
         <title>{pageInfo?.name}</title>
       </Head>
-      <Header socials={socials}/>
-      <section id="hero" className='snap-start'>
-         <Hero pageInfo={pageInfo} />
+      <Header socials={socials} />
+      <section id='hero' className='snap-start'>
+        <Hero pageInfo={pageInfo} />
       </section>
-      <section id="about" className='snap-center'>
+      <section id='about' className='snap-center'>
         <About pageInfo={pageInfo} />
       </section>
-      <section id="experience" className='snap-center'> 
-        <WorkExperience experience={experience}/>
+      <section id='experience' className='snap-center'>
+        <WorkExperience experience={experience} />
       </section>
-      <section id="skills" className='snap-start'>
-        <Skills skills={skills}/>
+      <section id='skills' className='snap-start'>
+        <Skills skills={skills} />
       </section>
-      <section id="projects" className='snap-start'>
-        <Projects projects={projects}/>
+      <section id='projects' className='snap-start'>
+        <Projects projects={projects} />
       </section>
-      <section id="contact" className='snap-start'>
-        <Contact/>
+      <section id='contact' className='snap-start'>
+        <Contact />
         {/* <Contact pageInfo={pageInfo} /> */}
       </section>
-      <Link href="#hero">
-      <footer className='sticky bottom-5 w-full cursor-pointer'>
-        <div className='flex items-center justify-center'>
-          <img src="https://i.imgur.com/3KNTWBj.png" alt="home" className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'/>
-        </div>
-      </footer>
+      <Link href='#hero'>
+        <footer className='sticky bottom-5 w-full cursor-pointer'>
+          <div className='flex items-center justify-center'>
+            <img
+              src='https://i.imgur.com/3KNTWBj.png'
+              alt='home'
+              className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
+            />
+          </div>
+        </footer>
       </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-     const pageInfo: PageInfo = await fetchPageInfo();
-     const experience: Experience[] = await fetchExperience();
-     const skills: Skill[] = await fetchSkills();
-     const projects: Project[] = await fetchProjects();
-     const socials: Social[] = await fetchSocials();
-      return {
-        props:{
-          pageInfo,
-          experience, 
-          skills,
-          projects,
-          socials
-        },
-        revalidate:10,
-      };
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experience: Experience[] = await fetchExperience();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocials();
+  return {
+    props: {
+      pageInfo,
+      experience,
+      skills,
+      projects,
+      socials,
+    },
+    revalidate: 10,
+    fallback: false,
+  };
 };
