d077486c8b8f9959143a3be7cbeedc436df9ce8b
kim <kim_man@live.com>
2022-10-20 20:10:55 +1100

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,8 @@ type Props = {
   socials: Social[];
 };
 
-const Home = ({ pageInfo, experience, skills, projects, socials }: Props) => {
+const Home = ({ experience, skills, projects, socials }: Props) => {
+  // const Home = ({ pageInfo, experience, skills, projects, socials }: Props) => {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
       {/* <Head>
