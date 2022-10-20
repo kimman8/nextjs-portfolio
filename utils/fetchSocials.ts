07a07e3bbfd0acbe7986c7163c98c79877b3c8a9
kim <kim_man@live.com>
2022-10-20 20:24:41 +1100

    wip

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -1,8 +1,10 @@
-import { Social } from "../typings";
+import { Social } from '../typings';
 
 export const fetchSocials = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
-    const data = await res.json();
-    const socials: Social[] = data.socials;
-    return socials;
+  const res = await fetch(
+    `/${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`
+  );
+  const data = await res.json();
+  const socials: Social[] = data.socials;
+  return socials;
 };
