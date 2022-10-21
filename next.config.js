7de32fd77c4ac2d72e7941b265d0a0be4629201f
kim <kim_man@live.com>
2022-10-21 16:58:40 +1100

    wip

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // images: {
-  //   domains: ['cdn.sanity.io'],
-  // },
+  images: {
+    domains: ['cdn.sanity.io', 'localhost'],
+  },
 };
 
 module.exports = nextConfig;
